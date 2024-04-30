ce00d813a43e32ae6c35d90d376e9d1e5264a9e6
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2024-04-30 23:30:12 +0200

    comments

--- load.go
+++ load.go
@@ -5,32 +5,38 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
-func LoadImages() []*ebiten.Image{
-	assetPath := "./assets"
-	Images := []*ebiten.Image{}
+
+func LoadImages() map[string]*ebiten.Image {
+	//assets location
+	assetPath := "/assets"
+	//dict for easy texture access
+	Images := make(map[string]*ebiten.Image)
 	if build {
-        assetPath = GetCWDPath(assetPath) + "/assets"
-		fmt.Println("asset path:"+assetPath)
-    }
-	if true{
+		//get cwd of executable
+		assetPath = GetCWDPath(assetPath) + "/assets"
+		fmt.Println("asset path:" + assetPath)
+	}
+	if true {
+		//loading images in the directory and saving the to images
 		items, _ := os.ReadDir(assetPath)
 		for _, item := range items {
 			if !item.IsDir() {
-                fmt.Println(assetPath + "/" + item.Name())
-				appendImage,_,err:= ebitenutil.NewImageFromFile(assetPath + "/" + item.Name())
-				if err!= nil {
-                    log.Fatal(err)
-                }
-				Images = append(Images, appendImage)
-
+				fmt.Println(assetPath + "/" + item.Name())
+				appendImage, _, err := ebitenutil.NewImageFromFile(assetPath + "/" + item.Name())
+				if err != nil {
+					log.Fatal(err)
+				}
+				//like in PyKart, textures can be accessed with their file name
+				fmt.Println("loaded image:" + item.Name())
+				Images[item.Name()] = appendImage
 			}
 		}
 	}
 	return Images
-
 }
 func GetCWDPath(path string) string {
 	//loading files in the directory of the executable
--- main.go
+++ main.go
@@ -10,11 +10,12 @@ import (
 
 var build bool = true
 type Game struct{
-	images []*ebiten.Image
+	//go dict
+	Textures map[string]*ebiten.Image
 }
 func NewGame() *Game {
 	return &Game{
-        images: LoadImages(),
+        Textures: LoadImages(),
     }
 }
 func (g *Game) Update() error {
@@ -23,13 +24,8 @@ func (g *Game) Update() error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(screen, "Hello, World!")
-	xpos := 0
-	for _, image := range g.images {
-		op := &ebiten.DrawImageOptions{}
-		op.GeoM.Translate(float64(xpos), 0)
-		screen.DrawImage(image, op)
-        xpos += image.Bounds().Size().X
-	}
+	//referencing items by image name
+	screen.DrawImage(g.Textures["stone.png"], nil)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
