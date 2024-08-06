fe5c920ea6b4cc572f59e2010214c2ad557a1c95
Valdotorium <116170877+Valdotorium@users.noreply.github.com>
2024-08-06 10:02:38 +0200

    refactor

---
+++ shell/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+go build src/main.go
+src/gobird
---
+++ src/constants.go
@@ -0,0 +1,14 @@
+//this file stores constant values, such as file paths that need to be loaded
+package main
+func fetchGameImagePaths() []string {
+	imagePaths := []string{
+		"assets/stone.png",
+		"assets/packet.png",
+	    "assets/grass.png"}
+	return imagePaths
+}
+
+var GroundY int = 200
+var PlayerX int = 200
+var WindowWidth int = 720
+var WindowHeight int = 480
